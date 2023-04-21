5b8ff40a2cc11aa10cae92ec99963a31045f8b31
Badrion <66309776+Badrion@users.noreply.github.com>
2023-04-21 20:03:05 +0000

    Ajout de fonctions dans map.c (voir la déscription plus détaillé)
    -void res_Map(Map* caseMap):
    Sert a initialiser une seule case de type Map.
    
    -Map** createMap(int width, int height):
    Crée et envoie un pointeur sur un tableau a 2D de type Map qui va pouvoir servir de map.
    Va de pair avec la fonction freeMap().
    
    -void freeMap(Map** map, int w):
    Permet de liberer la mémoire prise par une map.
    
    -void create_initMap(Map **map, int w, int h):
    Initialise une map avec la fonction res_Map().
    
    -void printMap(Map **map, int w, int h):
    Affiche une map dans sa totalitée.

--- .ccls-cache/@home@runner@mainc-1/src@map.c
+++ .ccls-cache/@home@runner@mainc-1/src@map.c
@@ -1,17 +1,49 @@
 #include "map.h"
 
-Map** createMap(int width, int height){
-    Map **map = NULL;
+void res_Map(Map* caseMap) {
+  caseMap->nombre = 'A';
+}
 
-    if( !(map = calloc(width, width*sizeof(int*)))){
-        printf("ERREUR: pb avec malloc");
+
+
+Map **createMap(int width, int height) {
+  Map **map = NULL;
+
+  if (!(map = calloc(width, width * sizeof(int *)))) {
+    printf("ERREUR: pb avec malloc");
+  }
+
+  for (int x = 0; x < width; x++) {
+    if (!(map[x] = calloc(height, height * sizeof(int)))) {
+      printf("ERREUR: pb avec malloc");
     }
+  }
 
-    for(int x = 0; x<width; x++){
-        if( !(map[x] = calloc(height, height*sizeof(int)))){
-            printf("ERREUR: pb avec malloc");
-        }
+  return map;
+}
+
+void freeMap(Map **map, int w) {
+  for (int x = 0; x < w; x++) {
+    free(map[x]);
+  }
+  free(map);
+}
+
+
+
+void create_initMap(Map **map, int w, int h) {
+  for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+      res_Map(&(map[x][y]));
     }
+  }
+}
 
-    return map;
+void printMap(Map **map, int w, int h) {
+  for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+      printf("|%c", map[x][y].nombre);
+    }
+    printf("\n");
+  }
 }
--- src/map.h
+++ src/map.h
@@ -4,3 +4,18 @@
 typedef struct{
     int nombre;
 }Map;
+
+void res_Map(Map* caseMap);
+
+// Retourne un pointeur sur un tableau a deux dimention de type Map.
+// Le tableau se lit: map[x][y]
+Map** createMap(int width, int height);
+
+// Libère la mémoire prise par une map passée en paramètre.
+// IMPORTANT DE LE FAIRE POUR TOUTES LES MAPS
+void freeMap(Map** map, int w);
+
+
+void create_initMap(Map **map, int w, int h);
+
+void printMap(Map **map, int w, int h);
