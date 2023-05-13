5ec786df3ba71266b62c73570133623b4973de95
Grimkujow <130107205+Grimkujow@users.noreply.github.com>
2023-05-13 23:17:57 +0000

    Début joueur
    manque pleins de truc écrit en note dans game.c

--- src/game.h
+++ src/game.h
@@ -9,12 +9,19 @@ typedef struct{
   int x;
   int y;
 }Pos;
+//stats des objet, si nourritures heal cbm de vie, le reste est logique.
+typedef struct{
+  int pvbuff;
+  int atkbuff;
+  int heal;
+}buffobj;
 
 typedef struct{
   int nbobj;
   char tab[30];
   int nbmax;
   int nbact;
+  buffobj stats;
 }Obj;
 
 #endif
--- src/joueur.c
+++ src/joueur.c
@@ -3,20 +3,46 @@
 
 
 typedef struct{
-  Obj inv[];
-  int stockagefree;
-  int stockagetotal;
+  Obj Inv[];
+  int Stockagepris;
+  int Stockagetotal;
 }Inventaire;
 
-
+//entitee peux etre utilisé pour les mobs avec posibilité de laisser leurs inv au sol qd ils meurent?
 typedef struct{
   Pos Pjoueur;
   int Pv;
+  int Atk;
   Inventaire InvJoueur;
 }Entitee;
 
-int modifpv(dmgorheal){
-  Entitee Joueur;
-  Joueur->Pv =+ dmgorheal;
-  
+void modifpv(int dmgorheal, Entitee* x){
+  x->Pv =+ dmgorheal;
 }
+
+void modifatk(int buff, Entitee* x){
+  x->Atk =+ buff;
+}
+
+void iniInventaire(Inventaire* Inv, int size){
+  Inv->Inv[size];
+  Inv->Stockagepris = 0;
+  Inv->Stockagetotal = size;
+  for (int n; n<size ; n++){
+    Inv->Inv[n]->nbobj = 0;
+    Inv->Inv[n]->nbmax = 0;
+    Inv->Inv[n]->nbact = 0;
+    Inv->Inv[n]->buffobj->buffpv = 0;
+    Inv->Inv[n]->buffobj->buffatk = 0;
+    Inv->Inv[n]->buffobj->heal = 0;
+    for (int n; n<30 , n++){
+          Inv->Inv[n]->tab[n] = 0
+      }
+  }
+}
+
+//manque prendre un item dans l'inv
+// la barre main inventaire de ce que le mec utilise
+// fonction qui enlève et ajt l'atk / pv en fonction de si l'obj est équipé ou pas
+//fonction consommation d'un obj?
+//
